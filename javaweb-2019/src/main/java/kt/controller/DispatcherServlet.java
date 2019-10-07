9a1aa02a37e07ead7d877e406a952f8fe711eecd
Yong Joon Kwon <bly99@naver.com>
2019-10-07 23:18:01 +0900

    modify DispatcherServlet.java

--- javaweb-2019/src/main/java/kt/controller/DispatcherServlet.java
+++ javaweb-2019/src/main/java/kt/controller/DispatcherServlet.java
@@ -26,13 +26,13 @@ public class DispatcherServlet extends HttpServlet {
 
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		resp.setContentType("text/html); charset=UTF-8");
+		resp.setContentType("text/html; charset=UTF-8");
 		String servletPath = req.getServletPath();
 		
 		try {
 			String pageControllerPath = null;
 			
-			if ("/host/list.do".equals(servletPath)) {
+			if ("/hostList.do".equals(servletPath)) {
 				pageControllerPath = "/hostList";
 			}
 			
