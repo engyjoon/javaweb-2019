870d9ad738c4fbd4f431c9c5e69dc0cb7492f12a
Yong Joon Kwon <bly99@naver.com>
2019-10-07 21:17:47 +0900

    modify HostListServlet.java

--- javaweb-2019/src/main/java/kt/controller/HostListServlet.java
+++ javaweb-2019/src/main/java/kt/controller/HostListServlet.java
@@ -1,11 +1,7 @@
 package kt.controller;
 
 import java.io.IOException;
-import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.util.ArrayList;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -14,6 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import kt.dao.HostDAO;
 import kt.vo.Host;
 
 /**
@@ -22,11 +19,6 @@ import kt.vo.Host;
 @WebServlet("/hostList")
 public class HostListServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    
-	private static final String DB_URL = "jdbc:postgresql://192.168.56.101:5432";
-	private static final String DB_NAME = "zabbix";
-	private static final String DB_USER = "zabbix";
-	private static final String DB_PASS = "zabbix";
 	
     /**
      * @see HttpServlet#HttpServlet()
@@ -40,50 +32,12 @@ public class HostListServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
+		HostDAO hostDAO = new HostDAO();
 		
-		ArrayList<Host> listHost = new ArrayList<>();
-		
-		try {
-			Class.forName("org.postgresql.Driver");
-			conn = DriverManager.getConnection(DB_URL + "/" + DB_NAME, DB_USER, DB_PASS);
-		
-			StringBuffer query = new StringBuffer();
-			query.append("select hostid, host, status, available, name, flags ");
-			query.append("from hosts where status = '0' and flags = '0' ");
-			query.append("order by hostid");
+		request.setAttribute("listHost", hostDAO.selectHostList());
 			
-			pstmt = conn.prepareStatement(query.toString());
-			rs = pstmt.executeQuery();
-			
-			while(rs.next()) {
-				Host host = new Host();
-				host.setHostid(rs.getLong("HOSTID"));
-				host.setHost(rs.getString("HOST"));
-				host.setStatus(rs.getInt("STATUS"));
-				host.setAvailable(rs.getInt("AVAILABLE"));
-				host.setName(rs.getString("NAME"));
-				host.setFlags(rs.getInt("FLAGS"));
-				listHost.add(host);
-			}
-			
-			request.setAttribute("listHost", listHost);
-			
-			RequestDispatcher rd = request.getRequestDispatcher("/test03.jsp");
-			rd.forward(request, response);
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			try{
-				rs.close();
-				pstmt.close();
-				conn.close();
-			} catch(Exception e) {
-				e.printStackTrace();
-			}
-		}
+		RequestDispatcher rd = request.getRequestDispatcher("/test03.jsp");
+		rd.include(request, response);
 	}
 
 	/**
