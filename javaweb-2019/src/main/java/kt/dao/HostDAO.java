4f1d19a880d854a6866c92668c80dd624d8db908
Yong Joon Kwon <bly99@naver.com>
2019-10-08 21:37:39 +0900

    fix location of variable

--- javaweb-2019/src/main/java/kt/dao/HostDAO.java
+++ javaweb-2019/src/main/java/kt/dao/HostDAO.java
@@ -11,21 +11,20 @@ import kt.vo.Host;
 
 public class HostDAO {
 	
-	Connection conn = null;
-	PreparedStatement pstmt = null;
-	ResultSet rs = null;
-	
 	public List<Host> selectHostList() {
-		ArrayList<Host> listHost = new ArrayList<>();
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		StringBuffer query = new StringBuffer();
+		query.append("select hostid, host, status, available, name, flags ");
+		query.append("from hosts where status = '0' and flags = '0' ");
+		query.append("order by hostid");
+		
+		ArrayList<Host> result = new ArrayList<>();
 		
 		try {
 			conn = JdbcUtil.getConnection();
-		
-			StringBuffer query = new StringBuffer();
-			query.append("select hostid, host, status, available, name, flags ");
-			query.append("from hosts where status = '0' and flags = '0' ");
-			query.append("order by hostid");
-			
 			pstmt = conn.prepareStatement(query.toString());
 			rs = pstmt.executeQuery();
 			
@@ -37,7 +36,7 @@ public class HostDAO {
 				host.setAvailable(rs.getInt("AVAILABLE"));
 				host.setName(rs.getString("NAME"));
 				host.setFlags(rs.getInt("FLAGS"));
-				listHost.add(host);
+				result.add(host);
 			}
 
 		} catch (Exception e) {
@@ -48,6 +47,6 @@ public class HostDAO {
 			JdbcUtil.close(conn);
 		}
 		
-		return listHost;
+		return result;
 	}
 }
