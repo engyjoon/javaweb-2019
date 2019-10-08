c5b617afca762111f54d6f98cce13c4d20c95712
Yong Joon Kwon <bly99@naver.com>
2019-10-08 22:36:10 +0900

    create History

---
+++ javaweb-2019/src/main/java/kt/vo/History.java
@@ -0,0 +1,20 @@
+package kt.vo;
+
+public class History {
+
+	private Object clock;
+	private Object value;
+	
+	public Object getClock() {
+		return clock;
+	}
+	public void setClock(Object clock) {
+		this.clock = clock;
+	}
+	public Object getValue() {
+		return value;
+	}
+	public void setValue(Object value) {
+		this.value = value;
+	}
+}
