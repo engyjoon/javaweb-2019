f09b5d828eb762e5a5a46552bf8effd118ac08cd
Yong Joon Kwon <bly99@naver.com>
2019-10-09 15:31:31 +0900

    delete response.setContentType

--- javaweb-2019/src/main/java/kt/controller/zabbix/ItemListServlet.java
+++ javaweb-2019/src/main/java/kt/controller/zabbix/ItemListServlet.java
@@ -30,6 +30,8 @@ public class ItemListServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html; charset=UTF-8");
+		
 		ItemDAO itemDAO = new ItemDAO();
 		
 		Host host = new Host();
